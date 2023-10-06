fc75c86221be243c5ad3d8c6b9cf0da9de30c544
salmansheri <sheriffsalman00@gmail.com>
2023-10-06 07:42:25 +0530

    feat: mobile responsive

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,5 +1,6 @@
 import Poster from "@/components/ui/poster";
 import Carousal from "@/components/ui/swiper/swiper";
+import ThumbnailSwiper from "@/components/ui/swiper/thumbnail-swiper";
 import Thumbnail from "@/components/ui/thumbnail";
 import { images, thumbnailImages } from "@/lib/constants/data";
 
@@ -7,7 +8,7 @@ export default function Home() {
   return (
     <section>
       <Carousal className="w-full h-[80vh]" images={images} />
-      <div className="lg:px-38 md:px-32 px-10 h-[40vh]  mt-10 grid grid-cols-8 w-full overflow-x-auto gap-4">
+      <div className="lg:px-38 md:px-32 px-10 h-[40vh]  mt-10 lg:grid grid-cols-8 w-full overflow-x-auto gap-4 hidden">
         {thumbnailImages.map((image) => (
           <Thumbnail
             label={image.label}
@@ -16,6 +17,12 @@ export default function Home() {
             src={image.src}
           />
         ))}
+      </div>
+      <div className="lg:hidden">
+        <ThumbnailSwiper
+          className="w-full  h-[200px] cursor-pointer"
+          thumbnailImages={thumbnailImages}
+        />
       </div>
       <Poster />
     </section>
